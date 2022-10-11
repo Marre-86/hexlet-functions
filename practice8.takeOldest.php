1cf64227b447c00e5c351203eb2e6ca5ffe86c2a
Artem Pokhiliuk <timtimoha@yandex.ru>
2022-10-11 11:44:22 +0700

    First Commit

---
+++ practice8.takeOldest.php
@@ -0,0 +1,27 @@
+<?php
+namespace App\Users;
+
+use Funct;
+use Funct\Strings;
+
+use function Funct\Collection\firstN;
+
+$users = [
+    ['name' => 'Tirion', 'birthday' => '1988-11-19'],
+    ['name' => 'Sam', 'birthday' => '1999-11-22'],
+    ['name' => 'Rob', 'birthday' => '1975-01-11'],
+    ['name' => 'Sansa', 'birthday' => '2001-03-20'],
+    ['name' => 'Tisha', 'birthday' => '1992-02-27']
+];
+
+function takeOldest($array, $num = 1) {   
+    $help = fn($a, $b) => $a['birthday'] <=> $b['birthday'];
+    usort($array, $help);
+    print_r($array);
+}
+
+// takeOldest($users);
+// echo firstN([1, 2, 3]);
+
+echo Funct\firstValue(null, null, 'foo_bar'); // => 'foo_bar'
+?>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'study-project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'marre/hexlet-functions',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'funct/funct' => array(
+            'pretty_version' => '1.5.0',
+            'version' => '1.5.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../funct/funct',
+            'aliases' => array(),
+            'reference' => '3f01f3f8f13c9f6b02a43b3ce65da141fdce3e40',
+            'dev_requirement' => false,
+        ),
+        'marre/hexlet-functions' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'study-project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
